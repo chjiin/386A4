025d07872dee81e88a745926de98e994220a1a8e
Pixels081 <87950265+Pixels081@users.noreply.github.com>
2021-07-27 13:07:55 -0400

    Update 190903900_170631720_a04.c

--- 190903900_170631720_a04.c
+++ 190903900_170631720_a04.c
@@ -1,62 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-
+	setbuf(stdout, NULL);
 	printf("test\n");
 	//first initialize all variables
+
 	int n = 3; //how many processes
 	int m = 5; //how many resource types
 	int i, j; //i loops through processes. j loops through resources
-	
-	
+	char line[64];
+
 	//initialize the 4 main arrays
-	
-	int max[][];
-	for(i=0; i<n; i++) {
-		for(j=0;j<m;j++) {
-			max[i][j] = 0;
+
+	int avail[n]; //this is set to be equal to the arguments
+	for (i = 0; i < n; i++) {
+		avail[i] = 6;
+	}
+
+	int max[n][m]; //initialized to sample_in
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			max[i][j] = 3; //----------------temporarily hard coded during testing. fix later
 		}
 	}
-	
 
 	//start the command entry loop
 	int cont = 1;
 
-	while (cont){
+	while (cont) { //the main loop
 
 		//take input
-		char line[64];
-
-		scanf("%[^\n]", line);
+		scanf("%s", &line);
 
 		//parse input
-		//the input can be one word or one word followed by ints. first check if its a one word command
-		if(strcmp(line, "status") == 1){
+		//the input can be one word or one word followed by ints. first check one word commands, the RL & RQ
+		if (strcmp(line, "status") == 1) {
 
-		} else if(strcmp(line, "Exit") == 1){
+		} else if (strcmp(line, "Exit") == 0) {
 			//Exit code----------------------------------------------------------
 			printf("Now Exiting Program\n");
 			cont = 0;
-		} else if(strcmp(line, "Status") == 1){
+		} else if (strcmp(line, "Status") == 0) {
 			//status display code------------------------------------------------
-
-		} else if(strcmp(line, "Run") == 1){
+			printf("Status display code\n");
+		} else if (strcmp(line, "Run") == 0) {
 			//run code-----------------------------------------------------------
-
+			printf("run code\n");
 		} else {//one word commands have been checked, now check if command is RL or RQ
 
-			char* bit = strtok(line, " ");
-			if (strcmp(bit, "RQ") == 1){
+			char *bit = strtok(line, " ");
+			if (strcmp(bit, "RQ") == 0) {
 				//Process Request Code-------------------------------------------
-				
-				
-				
-				
-			} else if (strcmp(bit, "RL") == 1){
-				//Process Release code-------------------------------------------
-			}
+				for (i = 0; i < n; i++) {
+					char *bit = strtok(line, " ");
+				}
 
+			} else if (strcmp(bit, "RL") == 0) {
+				//Process Release code-------------------------------------------
+			} else {
+				printf("Invalid Input\n");
+			}
+			printf("Looping now\n");
+			cont = 0;
 		}
-		return 0;
 	}
 
+	return 0;
 }
