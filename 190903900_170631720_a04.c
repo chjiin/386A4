3652322868ab1a9ce9ab62e7a4a130acd2274990
Pixels081 <87950265+Pixels081@users.noreply.github.com>
2021-07-27 15:13:48 -0400

    Update 190903900_170631720_a04.c

--- 190903900_170631720_a04.c
+++ 190903900_170631720_a04.c
@@ -2,22 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-int RQ( avail, max, allo, need, req, process) {
+//global variables
+int n;
+int m;
 
+int RQ( avail, max, allo, need, line) { //execute request algorithm and safety algorithm if needed
+	int process;
+	int req[m];
+	int i;
+	int j;
+	char *bit = strtok(line, " ");
+
+	process = atoi(bit);
+	for (j = 0; j < m; j++) {
+		bit = strtok(line, " ");
+		req[j] = atoi(bit);
+	}
+	if (RQ(avail, max, allo, need, req, process) == 0) {
+		printf("State is safe, and request is satisfied");
+	} else {
+		printf("Request denied");
+	}
+	return 0;
 	return 0;
 }
 
 int main() {
 	setbuf(stdout, NULL);
 	printf("test\n");
-	//first initialize all variables
+	//first initialize all variables. right now some are hard coded, until file input and argument input is coded
 
-	int n = 3; //how many processes
-	int m = 5; //how many resource types
+	n = 3; //how many processes
+	m = 5; //how many resource types
 	int i, j; //i loops through processes. j loops through resources
 	char line[64];
+	char *bit;
+	char c;
 	int process;
 	int req[m];
+	int flag;
 
 	//initialize the 4 main arrays
 
@@ -47,15 +70,20 @@ int main() {
 		}
 	}
 
-	//start the command entry loop
 	int cont = 1;
 
-	while (cont) { //the main loop
+	while (cont) { //------------the main loop
 
-		//take input
-		scanf("%s", &line);
+		//-----------take input
+		//scanf("%s", &line); scanf doesnt read whole line
+		scanf("%[^\n]%*c", line);
+		printf("input is -%s- and length is %d\n", line, strlen(line));
 
-		//parse input
+		line[strlen(line) - 1] = '\0';
+
+		printf("input is -%s-\n", line);
+
+		//----------parse input
 		//the input can be one word or one word followed by ints. first check one word commands, the RL & RQ
 		if (strcmp(line, "status") == 1) {
 
@@ -70,20 +98,37 @@ int main() {
 			//run code-----------------------------------------------------------
 			printf("run code\n");
 		} else {//one word commands have been checked, now check if command is RL or RQ
-
-			char *bit = strtok(line, " ");
+			bit = strtok(line, " ");
 			if (strcmp(bit, "RQ") == 0) {
-				//Process Request Code-------------------------------------------
-				bit = strtok(line, " ");
-				process = atoi(bit);
-				for (j = 0; j < m; j++) {
-					bit = strtok(line, " ");
-					req[j] = atoi(bit);
+				printf("RQ recognized\n");
+				
+				//-----------------------------check if there are the right number of only integers, then call fxn
+				i = 0;//counting number
+				j = 0;
+				flag = 1; //is the command valid
+				bit = strtok(NULL, " ");
+				while (bit != NULL && flag == 1) { //for every number
+					printf("checking bit %s\n", bit);
+					//printf( " %s\n", bit );
+					c = bit[i];
+					for (i = 0; i < strlen(bit); i++) {	//for every character
+						if (isdigit(c) != 1) {//if any single character is not a digit, we know the command is invalid
+							flag = 0;
+						}
+						c = bit[i];
+					}
+					bit = strtok(NULL, " ");
+					j++;
 				}
-				if (RQ(avail, max, allo, need, req, process) == 0) {
-					printf("State is safe, and request is satisfied");
-				} else {
-					printf("Request denied");
+				if (flag == 1 && j == (1 + m)) {//if every input was a number and there are the correct amount of numbers, proceed with command
+					printf("valid command");
+					/*
+					 if (RQ(avail, max, allo, need, line) == 0) {
+					 printf("RQ returned valid");
+					 } else {
+					 printf("RQ did not return valid");
+					 }
+					 */
 				}
 
 			} else if (strcmp(bit, "RL") == 0) {
@@ -92,7 +137,7 @@ int main() {
 				printf("Invalid Input\n");
 			}
 			printf("Looping now\n");
-			cont = 0;
+			//cont = 0;
 		}
 	}
 
